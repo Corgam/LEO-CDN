537c230d483521cbf7343bb1ff2a073cec25a92c
Corgam <emil.balicki2@gmail.com>
2021-05-28 11:15:16 +0200

    Fixed stardust

--- stardust/stardust.py
+++ stardust/stardust.py
@@ -7,28 +7,92 @@ import requests
 import json
 import http.client
 
-# GET URL
-BASE_URL = "http://172.26.9.3:5000"
+# Global variables
+GST_ID = "gst-0"
+
+# GET URLs
+BASE_URL = "http://172.26.8.3:5000"
 
 KEYGROUPS = BASE_URL + "/getKeygroups"
 INIT = BASE_URL + "/initializeKeygroup"
-ADD_DATA = BASE_URL + "/setData/test2/testid"
-ADD_MANAGE = BASE_URL + "/addToKeygroup"
-ADD_MANAGE2 = BASE_URL + "/addKeygroup"
-GETVALUE = BASE_URL + "/getValue/manage/addresses"
-GETVALUE2 = BASE_URL + "/getValue/manage/positions"
+ADD_DATA = BASE_URL + "/addData/test/testid"
+GETVALUE = BASE_URL + "/getValue/testid"
 
 LOCATION = BASE_URL + "/getLocation"
 SETLOCATION = BASE_URL + "/setLocation"
 POSITIONS = BASE_URL + "/positions"
-ADD_ROLE = BASE_URL + "/addRoles/manage"
-ADD_ROLE2 = BASE_URL + "/addRole/manage"
-APPEND = BASE_URL + "/appendData/manage/addresses"
 
+class HTTPRequest:
+    """
+    This class holds all the information about a single HTTP Request.
+
+
+    Parameters
+    ----------
+    URL: str
+    heads: dict
+    """
+    def __init__(self, URL, heads):
+        self.URL = URL
+        self.heads = heads
+
+    @classmethod
+    def fromString(self, reqString):
+        """
+        Sets the URL and heads dic from given string.
+        There is no error check.
+
+        Parameters
+        ----------
+        reqString : string
+            string containing url and heads
+
+        Returns
+        -------
+
+        """
+        lines = str(reqString).split('\n')
+        # Prepare url
+        url = lines[0]
+        url = url.replace("GET ","")
+        url = url.replace(" HTTP/1.1","")
+        # Prepeare headers
+        heads = {}
+        for line in lines:
+            if line == lines[0] or line == "":
+                continue
+            else:
+                # Fill in the headers
+                parts = line.split(": ")
+                heads[parts[0]] = parts[1]
+        return HTTPRequest(url,heads)
+
+    def getURL(self):
+        """
+        Returns the URL of the HTTP Request
+
+        Returns
+        -------
+        URL: string
+
+        """
+        return self.URL
+
+    def getHeads(self):
+        """
+        Returns the dict of heads of the HTTP Request
+
+        Returns
+        -------
+        heads: dic
+
+        """
+        return self.heads
 
 #########################
 ## Internal functions  ##
 #########################
+
 
 # Read the requests from the txt file and save them in array
 def readRequests():
@@ -40,152 +104,117 @@ def readRequests():
             # If empty line is encountered
             if line == "\n":
                 #Create an string 
-                reqsList.append(req)
+                reqsList.append(HTTPRequest.fromString(req))
                 req = ""                
             else:
                 req = req + line;
         # Add the last request after EOF
-        reqsList.append(req)
+        reqsList.append(HTTPRequest.fromString(req))
         return reqsList
 
-# Send all requests 
-def sendRequests(reqsList, ip, port):
-    # Create a connection
-    conn = http.client.HTTPConnection(ip,port)
+
+# Choose the best satelitte to send the HTTP requests to, by communicating with the coordinator
+def connectToTheBestSatellite():
+    # Communicate with the Coordinator to choose the best satellite.
+    coordConn = http.client.HTTPConnection("172.26.5.1","9001")
+    coordConn.request(method="GET",url=f"/best_satellite/{GST_ID}")
+    # Get the response
+    res = coordConn.getresponse()
+    # Extract ip and port
+    data = repr(res.read())
+    ip,port = data.split(":")
+    # Return connection to the best satellite
+    return http.client.HTTPConnection(ip,port);
+
+
+# Send all requests to the best satellite
+def sendRequests(reqsList):
+    # Create a connection to the best satellite
+    conn = connectToTheBestSatellite()
     # Send all requests
     for req in reqsList:
-        lines = str(req).split('\n')
-        # Prepare url
-        url = lines[0]
-        url = url.replace("GET ","")
-        url = url.replace(" HTTP/1.1","")
-        # Prepeare headers
-        head = {}
-        for line in lines:
-            if line == lines[0] or line == "":
-                continue
-            else:
-                # Fill in the headers
-                parts = line.split(": ")
-                head[parts[0]] = parts[1]
         # Send the request
-        conn.request(method="GET",url=url,headers=head)
+        conn.request(method="GET",url=req.getURL(),headers=req.getHeads())
         # Get response
         response = conn.getresponse()
         print(f"Status: {response.status} and reason: {response.reason}")
 
+
 # Main function, run on startup
 if __name__ == "__main__":
 
-    # # Read all requests from the file
-    # reqsList = readRequests()
-    # print("Sending all HTTP requests...")
-    # sendRequests(reqsList,"172.26.8.3","5000")
+    # Read all requests from the file
+    reqsList = readRequests()
+    print(f"Sending all {len(reqsList)} HTTP requests...")
+    sendRequests(reqsList)
 
-    # # Old functionality, to test press enter
-    # input("\nPress ENTER to continue and start testing custom HTTP requests")
+    # Old functionality, to test press enter
+    input("\nPress ENTER to continue and start testing custom HTTP requests")
 
-    # # Get all keygroups of node 1
-    # print('Get all keygroups..')
-    # r = requests.get(url=KEYGROUPS)
+    # Get all keygroups of node 1
+    print('Get all keygroups..')
+    r = requests.get(url=KEYGROUPS)
 
-    # print(r.text)
-    # print('-------------------------')
+    print(r.text)
+    print('-------------------------')
 
-    # # Initialize keygroup test
-    # print('Initialize keygroup..')
-    # r = requests.post(url=INIT, data='test2')
-    # print(r.text)
-    # print('-------------------------')
+    # Initialize keygroup test
+    print('Initialize keygroup..')
+    r = requests.post(url=INIT, data='test')
+    print(r.text)
+    print('-------------------------')
 
-    # # Add value
-    # print('Add data..')
-    # headers = {'Content-type': 'application/json'}
-    # data = {
-    #     'testx': '123'
-    # }
-    # r = requests.post(url=ADD_DATA, data=json.dumps(data), headers=headers)
-    # print(r.text)
-    # print('-------------------------')
+    # Add value
+    print('Add data..')
+    headers = {'Content-type': 'application/json'}
+    data = {
+        'testx': '123'
+    }
+    r = requests.post(url=ADD_DATA, data=json.dumps(data), headers=headers)
+    print(r.text)
+    print('-------------------------')
 
-    # print('joining manage')
-    # data = {
-    #             'satellite': "satellite1",
-    #             'keygroup': "manage",
-    #             'fred' : "fred1"
-    #         }
-    # r = requests.post(url=ADD_MANAGE, data=json.dumps(data))
-    # print(r.text)
-    # print('-------------------------')
-
-    # data = {
-    #             'satellite': "satellite1",
-    #             'keygroup': "manage",
-    #             'fred' : "fred1"
-    #         }
-    # r = requests.post(url=ADD_MANAGE2, data=json.dumps(data))
-    # print(r.text)
-    # print('-------------------------')
-
-    # print('Set role')
-    # data = {
-    #             'node': "fred",
-    #             'level': 1
-    #         }
-    # r = requests.post(url=ADD_ROLE2, data=json.dumps(data))
-    # print(r.text)
-
-
-    # data = "http://172.26.8.3:5000/"
-    # r = requests.post(url=APPEND, data=data)
-    # print(r.text)
-    # print('-------------------------')
     # Get value
     print('Get value..')
     r = requests.get(url=GETVALUE)
     print(r.text)
     print('-------------------------')
-    r = requests.get(url=GETVALUE2)
-    print(r.text)
+
+    ###################
+    ## Position test ##
+    ###################
+
+    # Get latest position of node
+    print('Get node1 location..')
+    r = requests.get(url=LOCATION)
+
+    position = None
+    if not r.text.startswith("<!DOCTYPE HTML PUBLIC "):
+        print(r.text)
+        # Change coordinates
+        position = json.loads(r.text.replace("'", '"'))
+        position["x"] += 1
+        position["y"] += 1
+        position["z"] += 1
+
+    else:
+        print('error')
+        
     print('-------------------------')
 
-    
+    if not position == None:
+        # Changes position of node 1
+        print('Change position..')
+        r = requests.post(url=SETLOCATION, data=json.dumps(position))
 
-    ##################
-    # Position test ##
-    ##################
+        # print response
+        print(r.text)
+        print('')
+        print('-------------------------')
 
-    # # Get latest position of node
-    # print('Get node1 location..')
-    # r = requests.get(url=LOCATION)
+        # Get latest position of node 1
+        print('Get all positions..')
+        r = requests.get(url=POSITIONS)
 
-    # position = None
-    # if not r.text.startswith("<!DOCTYPE HTML PUBLIC "):
-    #     print(r.text)
-    #     # Change coordinates
-    #     position = json.loads(r.text.replace("'", '"'))
-    #     position["x"] += 1
-    #     position["y"] += 1
-    #     position["z"] += 1
-
-    # else:
-    #     print('error')
-        
-    # print('-------------------------')
-
-    # if not position == None:
-    #     # Changes position of node 1
-    #     print('Change position..')
-    #     r = requests.post(url=SETLOCATION, data=json.dumps(position))
-
-    #     # print response
-    #     print(r.text)
-    #     print('')
-    #     print('-------------------------')
-
-    #     # Get latest position of node 1
-    #     print('Get all positions..')
-    #     r = requests.get(url=POSITIONS)
-
-    #     # print response
-    #     print(r.text)
+        # print response
+        print(r.text)
