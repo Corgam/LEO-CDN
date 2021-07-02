d301a580a642c9aaea6dc66644c7f1f458363aaa
ouzu <ouzu@pm.me>
2021-07-02 16:18:37 +0200

    update download script

--- cloud-setup/microvm/dl-microvm.sh
+++ cloud-setup/microvm/dl-microvm.sh
@@ -2,4 +2,5 @@
 
 wget https://s3.amazonaws.com/spec.ccfc.min/img/hello/kernel/hello-vmlinux.bin
 
-wget -O rootfs.ext4 https://ipfs.io/ipfs/Qme65F6fcj8xd9EbixzYrXex9PKy52o6ZirxgaV8NT35D2
+wget -O rootfs-gst.ext4 https://ipfs.io/ipfs/QmbxN1g5D5wGs1kAW4NrNfcAATJoTteKAEBdRem9AzKYN6
+wget -O rootfs-sat.ext4 https://ipfs.io/ipfs/QmRv3HR2uqai9nLyzUDvKdccNSfb5iEN4jqzJpgrBbWNgK
