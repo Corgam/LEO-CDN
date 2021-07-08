c3efef70f11f93e49ba2cabbabdde2efe5355072
ouzu <ouzu@pm.me>
2021-07-08 18:54:54 +0200

    update kernel & rootfs links

--- cloud-setup/microvm/dl-microvm.sh
+++ cloud-setup/microvm/dl-microvm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget https://s3.amazonaws.com/spec.ccfc.min/img/hello/kernel/hello-vmlinux.bin
+wget -O vmlinux-5.10.bin https://ipfs.io/ipfs/QmXs2QPKf4F97rU72Rgsy1yR9sfiw4vdmWe7YxEnY4fsfc
 
-wget -O rootfs-gst.ext4 https://ipfs.io/ipfs/QmbxN1g5D5wGs1kAW4NrNfcAATJoTteKAEBdRem9AzKYN6
-wget -O rootfs-sat.ext4 https://ipfs.io/ipfs/QmRv3HR2uqai9nLyzUDvKdccNSfb5iEN4jqzJpgrBbWNgK
+wget -O rootfs-gst.tar.zst https://ipfs.io/ipfs/QmaKjS45vowo4r5EEHHUF9wArbJYHZHwPzVG7w6TRhHxuX
+wget -O rootfs-sat.tar.zst https://ipfs.io/ipfs/QmbjJHNYEKPZz2i2yG3YgwicXWPE4NLGTSHKeNsQHiBBJJ
